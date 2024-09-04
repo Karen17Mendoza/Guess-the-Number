b780e9923364e138c0e4e9f910bcffd3a8a1cc32
Karen17Mendoza <melanie171095@gmail.com>
2024-09-04 12:19:26 -0500

    adds changes in some files

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,13 +1,22 @@
 import java.util.Random;
 
 public class ComputerPlayer extends Player {
-    public ComputerPlayer(String name) {
+    private Random random;
+
+    // Constructor que acepta un Random para pruebas
+    public ComputerPlayer(String name, Random random) {
         super(name);
+        this.random = random;
+    }
+
+    // Constructor para uso normal
+    public ComputerPlayer(String name) {
+        this(name, new Random()); // Usa un Random real por defecto
     }
 
     @Override
     public int makeGuess() {
-        int guess = new Random().nextInt(100) + 1;
+        int guess = random.nextInt(100) + 1;
         System.out.println(name + " adivina: " + guess);
         addGuess(guess); // Guardar suposición en la lista
         return guess;
